100702a835bf08bbce4e0257f00591b7058a1450
Sakshi Goyal <sakshigoyal718@gmail.com>
2021-06-07 18:25:12 +0530

    updated screen toggle

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React,{ Component, useState } from 'react';
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 import "codemirror/mode/xml/xml";
@@ -12,15 +12,19 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 
 const Editor=({name, value, language, onChange})=>{
+    const[isOpen, setIsOpen]=useState(true);
     function handleChange(editor, data, value){
         onChange(value);
     }
     return(
-        <div className="split-container">
+        <div className={`split-container ${isOpen? '':'collapsed'}`}>
             <div className="split-title">
                 {name}
-                <button type="button" className="editor-button">
-                <FontAwesomeIcon icon={faExpandAlt} />
+                <button type="button" 
+                className="editor-button"
+                onClick={()=>setIsOpen(prevVal=>!prevVal)}
+                >
+                <FontAwesomeIcon icon={isOpen? faCompressAlt :faExpandAlt} />
                 </button>
             </div>
             <Controlled value={value} 
